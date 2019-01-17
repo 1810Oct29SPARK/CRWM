bfd8581fcbbaca0a2808300aa20dd249bb431e27
Dmaw25 <44574708+Dmaw25@users.noreply.github.com>
2019-01-17 00:06:14 -0500

    Tests are donw

--- MedicalRPG/src/main/java/com/rev/controller/LoginController.java
+++ MedicalRPG/src/main/java/com/rev/controller/LoginController.java
@@ -13,6 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rev.beans.Player;
 import com.rev.service.LoginService;
 
+/**
+ * The Login Controller will take in the login information that the player
+ * then it will go through the loginservice and autherize that the credentils the user input matches any refrences in the data base
+ * finally if the information is returned properly we redirect the user to their profile page 
+ * @author Darius
+ *
+ */
 @Controller
 public class LoginController {
 
@@ -23,27 +30,26 @@ public class LoginController {
 		super();
 		this.autherize = loginService;
 	}
-
-	@GetMapping(value = "/login")
-	public String getStaticLogin() {
-		return "forward:/static/staticLogin.html";
+	
+	@GetMapping(value = "/auth")
+	public String getStaticLogin(@RequestBody MultiValueMap<String, String> formParams, Model m) {	
+		return handleForm(formParams,m);
 	}
 
 	@PostMapping(value = "/auth", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public String handleForm(@RequestBody MultiValueMap<String, String> formParams, Model m) {
 		System.out.println("form params recieved: " + formParams);
-
 		Player cred = autherize.isValidUser(formParams.getFirst("login-username"),
 				formParams.getFirst("login-password"));
 		if (cred == null) {
 			return "redirect:http://localhost:4200/login";
 		} else {
 			m.addAttribute("username", cred.getUsername());
-//			m.addAttribute("password", cred.getPassword());
+			m.addAttribute("password", cred.getPassword());
 			m.addAttribute("firstname", cred.getFirstname());
 			m.addAttribute("lastname", cred.getLastname());
 			m.addAttribute("score", cred.getScore());
-//			m.addAttribute("isdev", cred.getIsdev());
+			m.addAttribute("isdev", cred.getIsdev());
 			if (cred.getIsdev().equals("false")) {
 				return "redirect:http://localhost:4200/playerPage";
 			} else {
